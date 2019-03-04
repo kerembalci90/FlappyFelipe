53007a3c3376b2585305bccd79590c245954aaea
Kerem BALCI <kerembalci90@gmail.com>
2019-03-03 20:34:22 -0500

    - Setup scene and the scene background

--- FlappyFelipe/GameViewController.swift
+++ FlappyFelipe/GameViewController.swift
@@ -1,4 +1,3 @@
-//
 //  GameViewController.swift
 //  FlappyFelipe
 //
@@ -11,39 +10,35 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+        if let skView = self.view as? SKView {
+            if skView.scene == nil {
+                let aspectRatio = skView.bounds.size.height / skView.bounds.size.width
+                let scene = GameScene(size: CGSize(width: 320, height: 320 * aspectRatio))
                 
-                // Present the scene
-                view.presentScene(scene)
+                skView.showsFPS = false
+                skView.showsNodeCount = false
+                skView.showsPhysics = false
+                skView.ignoresSiblingOrder = true
+                
+                scene.scaleMode = .aspectFit
+                
+                skView.presentScene(scene)
             }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return [.portrait, .portraitUpsideDown]
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
